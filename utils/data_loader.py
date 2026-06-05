6680144af1764fa405487a454ce5803aff75acbe
spoorthijoladarashi <spoorthijoladarashi@gmail.com>
2026-06-05 23:30:51 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+def load_data():
+
+    holdings = pd.read_csv(
+        "data/MASTER_DATA_ENRICHED.csv"
+    )
+
+    signals = pd.read_csv(
+        "data/PREMIUM_CROSS_MARKET_SIGNALS.csv"
+    )
+
+    return holdings, signals
