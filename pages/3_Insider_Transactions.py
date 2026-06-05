0c2263b2c308a632ecd3728b7cffb0e4c25a7e59
spoorthijoladarashi <spoorthijoladarashi@gmail.com>
2026-06-05 23:32:50 +0530

    Update 3_Insider_Transactions.py

--- pages/3_Insider_Transactions.py
+++ pages/3_Insider_Transactions.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import plotly.express as px
 
+from utils.data_loader import load_data
+
+df,_ = load_data()
+
+st.title("Insider Transactions")
+
+if "transaction_code" in df.columns:
+
+    fig=px.histogram(
+        df,
+        x="transaction_code",
+        title="Buy/Sell Distribution"
+    )
+
+    st.plotly_chart(
+        fig,
+        use_container_width=True
+    )
