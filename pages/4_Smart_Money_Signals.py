f3de3d71dc5683442ce8bed72c93276e4f4d9484
spoorthijoladarashi <spoorthijoladarashi@gmail.com>
2026-06-05 23:33:12 +0530

    Update 4_Smart_Money_Signals.py

--- pages/4_Smart_Money_Signals.py
+++ pages/4_Smart_Money_Signals.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import plotly.graph_objects as go
 
+from utils.data_loader import load_data
+
+_,signals=load_data()
+
+st.title("Smart Money Signals")
+
+score = signals["signal_score"].mean()
+
+fig = go.Figure(
+go.Indicator(
+    mode="gauge+number",
+    value=score,
+    title={"text":"Smart Money Score"}
+))
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
+
+st.dataframe(
+    signals.sort_values(
+        by="signal_score",
+        ascending=False
+    )
+)
