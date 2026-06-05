865eceed45c88e0647c993be88bf94b4262ca7a7
spoorthijoladarashi <spoorthijoladarashi@gmail.com>
2026-06-05 23:33:33 +0530

    Update 5_AI_Insights.py

--- pages/5_AI_Insights.py
+++ pages/5_AI_Insights.py
@@ -1,1 +1,41 @@
+import streamlit as st
 
+from utils.data_loader import load_data
+
+df,_=load_data()
+
+st.title("AI Insights")
+
+avg=df["conviction_score"].mean()
+
+if avg>75:
+
+    st.success("""
+    Strong institutional
+    conviction detected.
+    """)
+
+elif avg>50:
+
+    st.warning("""
+    Moderate confidence
+    among institutions.
+    """)
+
+else:
+
+    st.error("""
+    Weak institutional
+    sentiment.
+    """)
+
+top = (
+    df.groupby("issuer_name")
+    ["market_value"]
+    .sum()
+    .idxmax()
+)
+
+st.info(
+    f"Highest institutional allocation: {top}"
+)
